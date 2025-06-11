14c144899e0627920d67a1df65b3b667d6119456
Wira Kusuma Phandawa <wirakusuma5566@gmail.com>
2025-06-11 18:13:20 +0700

    update

--- script.js
+++ script.js
@@ -15,21 +15,34 @@ generate.addEventListener('click', function() {
     localStorage.setItem("uang", angka); // Simpan ke localStorage
 });
 
-let autogene = document.getElementById("auto").addEventListener('click', auto);
+let autogene = document.getElementById("autogen").addEventListener('click', auto);
 
 function auto() {
     if (Number(money.innerHTML) >= 10) {
-    let interval = setInterval(function() {
+        let interval = setInterval(function() {
         let angka = Number(money.innerHTML);
         angka += 1;
         money.innerHTML = angka;
-        localStorage.setItem("uang", angka); // Simpan ke localStorage
-    }, 500);
-}
+        localStorage.setItem("uang", angka);
+        }, 500);
+    }
+    autogene.style.display = "none";
 }
 
 // Event untuk tombol reset
 reset.addEventListener('click', function() {
     money.innerHTML = 0;
     localStorage.setItem("uang", 0); // Reset data di localStorage
+});
+
+
+let user = document.getElementById("user");
+let modal = document.getElementById("modal");
+
+document.getElementById("login").addEventListener('click', function() {
+    let username = document.getElementById("username").value;
+    user.innerText=username;
+    modal.style.display="none";
+
+    
 });
