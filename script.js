3d1a28d019889a6d8a17728e342ea4f7c032d9ed
Wira Kusuma Phandawa <wirakusuma5566@gmail.com>
2025-12-11 20:55:54 +0700

    update

--- script.js
+++ script.js
@@ -1,83 +1,24 @@
 let generate = document.getElementById("generate");
 let money = document.getElementById("money");
 let reset = document.getElementById("reset");
+let currentMoney = 0;
 
-// Ambil data dari localStorage saat halaman dibuka
-let saved = localStorage.getItem("uang");
-if (saved !== null) {
-    money.innerHTML = saved;
-}
+
 
 generate.addEventListener('click', function() {
-    let angka = Number(money.innerHTML);
-    angka += 1;
-    money.innerHTML = angka;
-    localStorage.setItem("uang", angka); // Simpan ke localStorage
+    currentMoney += 1;
+    localStorage.setItem("savedMoney", currentMoney);
+    money.innerText = currentMoney;
 });
 
-
-let autogene1 = document.getElementById("autogen1");
-let autogene2 = document.getElementById("autogen2");
-let autogene3 = document.getElementById("autogen3");
-
-autogene1.addEventListener('click', auto1);
-autogene2.addEventListener('click', auto2);
-autogene3.addEventListener('click', auto3);
-
-function auto1() {
-    if (Number(money.innerHTML) >= 10) {
-        let interval = setInterval(function() {
-        let angka = Number(money.innerHTML);
-        angka += 1;
-        money.innerHTML = angka;
-        localStorage.setItem("uang", angka);
-        }, 500);
-    }
-    autogene1.style.display = "none";
-}
-function auto2() {
-    if (Number(money.innerHTML) >= 100) {
-        let interval = setInterval(function() {
-        let angka = Number(money.innerHTML);
-        angka += 1;
-        money.innerHTML = angka;
-        localStorage.setItem("uang", angka);
-        }, 500);
-    }
-    autogene2.style.display = "none";
-}
-function auto3() {
-    if (Number(money.innerHTML) >= 500) {
-        let interval = setInterval(function() {
-        let angka = Number(money.innerHTML);
-        angka += 1;
-        money.innerHTML = angka;
-        localStorage.setItem("uang", angka);
-        }, 500);
-    }
-    autogene3.style.display = "none";
+let savedMoney = localStorage.getItem("savedMoney");
+if (savedMoney) {
+    money.innerText = savedMoney;
+    currentMoney = parseInt(savedMoney);
 }
 
-// Event untuk tombol reset
-reset.addEventListener('click', function() {
-    money.innerHTML = 0;
-    localStorage.setItem("uang", 0); // Reset data di localStorage
-});
-
-// user login modal
-let user = document.getElementById("user");
-let modal = document.getElementById("modal");
-
-document.getElementById("login").addEventListener('click', function() {
-    let username = document.getElementById("username").value;
-    user.innerText=username;
-    modal.style.display="none";
-});
-
-// logout
-let logout = document.getElementById("logout");
-
-logout.addEventListener("click", function() {
-    user.innerHTML="";
-    modal.style.display="flex";
+reset.addEventListener("click", function() {
+    localStorage.setItem("savedMoney","0");
+    money.innerText = 0;
+    currentMoney = 0;
 });
